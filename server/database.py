c18b835940ec8130ca3fd9861cdf0e1a89e8f189
martyn-ukrainian <peacepeacecreation@gmail.com>
2026-07-10 14:32:56 +0300

    feat: externalize config via pydantic-settings and .env

--- server/database.py
+++ server/database.py
@@ -1,13 +1,13 @@
 from sqlmodel import SQLModel, create_engine
 
 from logger import get_logger
+from settings import settings
 
 logger = get_logger(__name__)
 
-sqlite_url = "sqlite:///agro.db"
-engine = create_engine(sqlite_url, connect_args={"check_same_thread": False})
+engine = create_engine(settings.database_url, connect_args={"check_same_thread": False})
 
 
 def init_db() -> None:
-    logger.info("Creating tables in %s", sqlite_url)
+    logger.info("Creating tables in %s", settings.database_url)
     SQLModel.metadata.create_all(engine)
